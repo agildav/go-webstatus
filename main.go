3fd9fa40367dcdd144fbe6e433e802ca7dcc9039
Alberto Gil <agildav@gmail.com>
2019-03-11 08:13:43 -0400

    Added concurrency

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
+	c := make(chan string)
 
 	links := []string{
 		"http://www.google.com",
@@ -16,18 +18,27 @@ func main() {
 	}
 
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for link := range c {
+		go func(l string) {
+			time.Sleep(5 * time.Second)
+			go checkLink(l, c)
+		}(link)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, "is down")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up")
+	c <- link
 	return
 }
